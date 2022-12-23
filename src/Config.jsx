2849e9c9c632bb10f908927f01ee8c8bab2a4f49
siva29061997 <siva29061997@gmail.com>
2022-12-23 07:55:26 +0530

    new

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,7 +1,2 @@
 export const env = {
-    api: "https://siva-blog-app.onrender.com/api"
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> b4b34ee051bc20e085aa60bb01782c86b4b309c8
+    api: "https://siva-blog-app.onrender.com/api"}
